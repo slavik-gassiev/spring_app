772680e916d785a1b203463e5307b780b21aacc1
slavik-gassiev <slavikgassiev1872002@gmail.com>
2024-10-01 07:32:13 +0300

    Added FirstController, SecondController

---
+++ src/main/java/sl/learn/spring/controllers/SecondController.java
@@ -0,0 +1,13 @@
+package sl.learn.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+
+    @GetMapping("/exit")
+    public String exitPage() {
+        return "second/exit";
+    }
+}
